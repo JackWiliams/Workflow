377571bd3c3870d644c00d4a91510d8581a671c1
KHAI NGO QUANG <quangkhai2609@gmail.com>
2020-06-10 17:48:54 +0700

    Update : Frontend

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ $('button').click(function(){
 $('.pop-up .close').click(function(){
   $('.pop-up').removeClass('open');
 });
+
+$('#submit_btn').click(function () {
+  Swal.fire('Oops...', 'Something went wrong!', 'error');
+  $('.pop-up').removeClass('open');
+})
